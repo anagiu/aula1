e7d7dd7452fec68da7c2131dacb36e824cbbd6b5
anagiu <126169431+anagiu@users.noreply.github.com>
2023-03-17 11:01:26 -0300

    excluindo user

--- usuario_apagar.php
+++ usuario_apagar.php
@@ -1,4 +1,33 @@
 <?php
 
 require('twig_carregar.php');
-echo $twig->render('usuario_apagar.html',);
+require('pdo.inc.php'); //conexão com o banco
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $id = $_POST['id'] ?? false;
+    if ($id) {
+        $sql = $pdo->prepare('UPDATE usuarios SET ativo = 0 WHERE id = ?');
+        $sql->execute([$id]);
+        
+    }
+
+header('location:usuarios.php');
+    die;
+
+}
+
+
+$id = $_GET['id'] ?? false;
+$sql = $pdo->prepare('SELECT * FROM usuarios WHERE id = ?');
+$sql->execute([$id]);
+$usuario = $sql->fetch(PDO::FETCH_ASSOC);
+
+
+
+
+echo $twig->render('usuario_apagar.html',[
+    'usuario' => $usuario,
+
+
+]);
